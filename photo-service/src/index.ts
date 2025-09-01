37ff481429af26e50661ce3c01b7e1545567850c
futantan <luckytantanfu@gmail.com>
2025-09-01 22:28:10 +1000

    Add authentication integration and environment variable updates for services

--- photo-service/src/index.ts
+++ photo-service/src/index.ts
@@ -26,6 +26,12 @@ router
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const auth_response = await env.AUTH.fetch(request.clone());
+
+		if (auth_response.status !== 200) {
+			return auth_response;
+		}
+
 		return router.fetch(request, env);
 	},
 } satisfies ExportedHandler<Env>;
